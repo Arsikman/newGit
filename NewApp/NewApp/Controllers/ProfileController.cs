23b1753827a2cf8b8499442c6c2c2ad67552e5d7
Vita Chubenko <vitachubenko11@gmail.com>
2018-04-03 21:54:44 +0300

    ProfileController

---
+++ NewApp/NewApp/Controllers/ProfileController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        // GET: Profile/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Profile/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
