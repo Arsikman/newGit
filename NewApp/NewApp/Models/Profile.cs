e26cddce6ab7d90cc7d7a9cec6c3924b478143f8
Vita Chubenko <vitachubenko11@gmail.com>
2018-04-03 21:16:31 +0300

    added Profile model

---
+++ NewApp/NewApp/Models/Profile.cs
@@ -0,0 +1,10 @@
+﻿namespace NewApp.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+        public string MobilePhone { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
